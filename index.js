043389e8b1bb08bb90d37e9fe1bf423dcda950d0
Chase <chasedemaster.tech@gmail.com>
2022-06-12 19:09:43 -0500

    commit tree a2d7dab98cd70ad0c97463badf12fade22ac3f34

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result;
+  if (distance <= 400) {
+    result = "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+    result = "That will be twenty bucks."
+  } else if (distance >= 2000 && distance < 2500) {
+    result = "I will gladly take your thirty bucks."
+  } else if (distance >= 2500) {
+    result = "No can do."
+  } 
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(item){
+  return (item === 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generousity){
+  let goodBye
+    if (generousity === 'generous'){
+    goodBye = 'Thank you so much.'
+    } else if (generousity === 'not as generous'){
+    goodBye = "Thank you."
+    } else {
+    goodBye = 'Bye.'
+    } 
+    return goodBye
 }
